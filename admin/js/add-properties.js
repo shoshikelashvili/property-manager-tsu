b1c6bedce6ac8f043d2d55e461a7b0eedb186aac
Rati.Shoshikelashvili6911@ens.tsu.edu.ge <Rati.Shoshikelashvili6911@ens.tsu.edu.ge>
2021-05-05 09:04:49 +0400

    adjusted custom fields panel display

---
+++ admin/js/add-properties.js
@@ -0,0 +1,16 @@
+//Customizing the Custom fields panel to fit our needs
+jQuery(window).load(function(){
+    var existing_metas = ["petsAllowed","property_area","property_bathrooms","property_bedrooms","property_id","property_location","property_price", "property_status", "property_year"]
+
+    jQuery('#the-list').children().each(function(){
+        var inputElement = jQuery(this).find(jQuery('input[type=text]'));
+        if(existing_metas.includes(inputElement.val()))
+        {
+            jQuery(this).css("display","none");
+        }
+    });
+
+
+    console.log(jQuery('#enternew').parent());
+    jQuery('#enternew').parent().click();
+});
