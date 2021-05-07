659da7c7718592b35e7ffe21dca71559726fb78b
Rati.Shoshikelashvili6911@ens.tsu.edu.ge <Rati.Shoshikelashvili6911@ens.tsu.edu.ge>
2021-05-07 09:27:15 +0400

    added the initial version of images slider

---
+++ public/js/property-details-view.js
@@ -0,0 +1,6 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+	new Splide( '#image-slider', {
+		'cover'      : true,
+		'heightRatio': 0.8,
+	} ).mount();
+} );
--- public/partials/property-details-view.php
+++ public/partials/property-details-view.php
@@ -13,7 +13,25 @@
  */
 
 wp_enqueue_style( 'property-details-view-css', plugin_dir_url( __FILE__ ) . '../css/property-details-view.css', array(), $this->version, 'all' );
-echo "test";
+wp_enqueue_style( 'splide-css', plugin_dir_url( __FILE__ ) . '../../vendor/splide/dist/css/splide.min.css', array(), $this->version, 'all' );
+wp_enqueue_script( 'property-details-view-js', plugin_dir_url( __FILE__ ) . '../js/property-details-view.js', array( 'jquery' ), $this->version, false );
+wp_enqueue_script( 'splide-js', plugin_dir_url( __FILE__ ) . '../../vendor/splide/dist/js/splide.min.js', array( 'jquery' ), $this->version, false );
+
+$property = get_post($this->property_id);
+$images = get_attached_media('image',$property);
+$property_data = get_post_meta($property);
+
 ?>
 
-<!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<div id="image-slider" class="splide">
+	<div class="splide__track">
+		<ul class="splide__list">
+        <?php 
+        foreach($images as $image)
+        {
+            echo '<li class="splide__slide"><img src="'.$image->guid.'"></li>';
+        }
+        ?>
+		</ul>
+	</div>
+</div>
