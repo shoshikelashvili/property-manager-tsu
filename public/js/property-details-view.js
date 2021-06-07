7217f6e939ad190dbc11cf643de9131e0343999c
Rati.Shoshikelashvili6911@ens.tsu.edu.ge <Rati.Shoshikelashvili6911@ens.tsu.edu.ge>
2021-06-08 01:08:39 +0400

    added go back functionality in property details view

--- public/js/property-details-view.js
+++ public/js/property-details-view.js
@@ -1,3 +1,14 @@
+function redirect()
+{
+	var url = window.location.href;
+	url = url.slice(0, -1);
+	var to = url.lastIndexOf('/');
+	to = to == -1 ? url.length : to + 1;
+	url = url.substring(0, to);
+	console.log('url', url);
+	window.location.replace(url)
+}
+
 document.addEventListener( 'DOMContentLoaded', function () {
 	new Splide( '#image-slider', {
 		'cover'      : true,
--- public/partials/property-details-view.php
+++ public/partials/property-details-view.php
@@ -164,3 +164,7 @@ $agent = get_user_by('slug',$agent_slug);
     ?>
     </ul>
 </div>
+<div class="back_to_properties" onClick="redirect()">
+        <span><?php _e('Back to the property listings', 'property-manager')?></span>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 0h512v512H0z" fill="#ffffff" fill-opacity="0"></path><g class="" transform="translate(0,0)" style=""><path d="M208.242 24.629l-52.058 95.205 95.207 52.059 17.271-31.586-42.424-23.198A143.26 143.26 0 0 1 256 114c78.638 0 142 63.362 142 142s-63.362 142-142 142-142-63.362-142-142c0-16.46 2.785-32.247 7.896-46.928l-32.32-16.16C82.106 212.535 78 233.798 78 256c0 98.093 79.907 178 178 178s178-79.907 178-178S354.093 78 256 78c-13.103 0-25.875 1.44-38.18 4.148l22.008-40.25-31.586-17.27zm104.27 130.379L247 253.275V368h18V258.725l62.488-93.733-14.976-9.984z" fill="#000000" fill-opacity="1"></path></g></svg>
+</div>
